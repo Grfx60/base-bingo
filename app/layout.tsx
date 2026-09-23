3060bfddf1867f7afa63eed6f94527a874bf840c
Grfx60 <mavigrfx@hotmail.com>
2026-09-23 16:15:15 +0300

    Update Base App ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
 
   other: {
     // Base Dashboard domain verification
-    "base:app_id": "6a86d5f66ea1f57fed333a51",
+    "base:app_id": "697e42fa2aafa0bc9ad8a2fd",
 
     "fc:miniapp": JSON.stringify(miniAppEmbed),
     "fc:frame": JSON.stringify(miniAppEmbed),
