ff95005dd0eb4860c6d46fff21fa6c7507f64b2c
Grfx60 <mavigrfx@hotmail.com>
2026-09-23 19:17:01 +0300

    add Base attribution data suffix

--- app/wagmi.ts
+++ app/wagmi.ts
@@ -7,20 +7,31 @@ import {
 
 import { base, baseSepolia } from "wagmi/chains";
 import { injected } from "@wagmi/core";
+import { Attribution } from "ox/erc8021";
+
+const DATA_SUFFIX = Attribution.toDataSuffix({
+  codes: ["bc_wsh2e7z6"],
+});
 
 export const config = createConfig({
   chains: [base, baseSepolia],
+
   connectors: [
     injected(),
   ],
+
   storage: createStorage({
     storage: cookieStorage,
   }),
+
   ssr: true,
+
   transports: {
     [base.id]: http(),
     [baseSepolia.id]: http(),
   },
+
+  dataSuffix: DATA_SUFFIX,
 });
 
 declare module "wagmi" {
